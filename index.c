0de90084f50206d46752d0163c9a1fdf3b0443d1
ruth-chirwa <chirwaruth647@gmail.com>
2024-05-22 09:10:31 -0400

    add

--- index.c
+++ index.c
@@ -1,9 +1,7 @@
-#include<stdio.h>
-#include "cs50.h"
+#include <stdio.h>
+
 int main(void)
 {
-printf("hello, Ruth\n");
-printf("how are you\n");
-printf("I am working towards my sofwtare engineering career!\n");
-return 0;
+   printf("hello world");
+    return 0;
 }
